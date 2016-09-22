dcfec91db598943fd53411f3931871e88ede07a8
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-21 21:28:33 -0500

    additional creep extensions

--- creep.extensions.js
+++ creep.extensions.js
@@ -1,39 +1,55 @@
 var creepExtensions = {
 	run: function(creep)
 	{
-        //Spawn
-        Creep.prototype.findClosestSpawn = function() {
-            return this.pos.findClosestByPath(FIND_MY_SPAWNS);
-        };
+		Creep.prototype.findClosestSpawn = function() {
+				return this.pos.findClosestByPath(FIND_MY_SPAWNS);
+		};
 
-        //Container
-        Creep.prototype.findClosestContainer = function() {
-            return this.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: function(c) {
-                    return c.structureType == STRUCTURE_CONTAINER && c.store.energy < c.storeCapacity;
-                }
-            });
-        };
+		Creep.prototype.findClosestPlaceToDumpEnergy = function() {
+				return this.pos.findClosestByPath(FIND_STRUCTURES, {
+						filter: (structure) => {
+								return (structure.structureType == STRUCTURE_EXTENSION ||
+												structure.structureType == STRUCTURE_SPAWN ||
+												structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+						}
+				});
+		};
 
-        Creep.prototype.findClosestContainerWithEnergy = function() {
-            return this.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: function(c) {
-                    return c.structureType == STRUCTURE_CONTAINER && c.store.energy < c.storeCapacity && c.store.energy > 0;
-                }
-            });
-        };
+		Creep.prototype.findClosestContainerThatIsNotFull = function() {
+				return this.pos.findClosestByPath(FIND_STRUCTURES, {
+						filter: function(c) {
+								return c.structureType == STRUCTURE_CONTAINER && c.store.energy < c.storeCapacity;
+						}
+				});
+		};
 
-        Creep.prototype.findClosestSource = function() {
-            return this.pos.findClosestByPath(FIND_SOURCES);
-        };
+		Creep.prototype.findClosestContainerWithEnergy = function() {
+				return this.pos.findClosestByPath(FIND_STRUCTURES, {
+						filter: function(c) {
+								return c.structureType == STRUCTURE_CONTAINER  && c.store.energy > 0;
+						}
+				});
+		};
 
-        Creep.prototype.findClosestStructureNeedingRepair = function() {
-            return this.pos.findClosestByPath(FIND_STRUCTURES, {filter: function(s) {return s.hits < (s.hitsMax*.7)}});
-        };
+		Creep.prototype.findSources = function() {
+					return this.room.find(FIND_SOURCES);
+		};
 
-        Creep.prototype.findClosestConstructionSite = function() {
-            return this.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
-        };
+		Creep.prototype.findClosestSource = function() {
+				return this.pos.findClosestByPath(FIND_SOURCES);
+		};
+
+		Creep.prototype.findClosestStructureNeedingRepair = function() {
+				return this.pos.findClosestByPath(FIND_STRUCTURES, {filter: function(s) {return s.hits < (s.hitsMax*.7)}});
+		};
+
+		Creep.prototype.findClosestConstructionSite = function() {
+				return this.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+		};
+
+		Creep.prototype.placeRoadUnderMe = function() {
+			return this.pos.createConstructionSite(STRUCTURE_ROAD);
+		};
 	}
 };
 
