4091eec508c26c923f9f18a915c361a29454a1f2
Jacob Cunningham <jacob.cunningham@live.com>
2016-09-21 21:31:38 -0500

    update miner class to use extensions

--- role.miner.js
+++ role.miner.js
@@ -1,12 +1,26 @@
 var helpers = require('global.helpers');
 
+var findSourceToHarvest = function(creep) {
+  var sources = creep.findSources();
+  var source = null;
+  _.forEach(sources, function(s) {
+    if(!_.any(Game.creeps, function(c) { return c.memory.targetSourceId == s.id})) {
+        console.log(JSON.stringify(s));
+      source = s;
+    }
+  });
+  return source;
+};
+
 var roleMiner = {
     create: function() {
         var tiers = [
             {body:[WORK,CARRY,MOVE]},
             {body:[WORK,WORK,CARRY,MOVE]},
             {body:[WORK,WORK,WORK,CARRY,MOVE]},
-            {body:[WORK,WORK,WORK,CARRY,MOVE,MOVE]}
+            {body:[WORK,WORK,WORK,WORK,CARRY,MOVE]},
+            {body:[WORK,WORK,WORK,WORK,WORK,CARRY,MOVE]},
+            {body:[WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE]}
         ];
 
         _.forEach(tiers, function(tier){
@@ -18,19 +32,28 @@ var roleMiner = {
     },
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
-            var source = helpers.findSource(creep);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
+            var source = null;
+            if(creep.memory.targetSourceId) {
+        			source = Game.getObjectById(creep.memory.targetSourceId);
+        		}
+            else {
+              source = findSourceToHarvest(creep);
+              creep.memory.targetSourceId = source.id;
             }
+
+          if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(source);
+          }
         }
         else {
-            var container = helpers.findClosestContainer(creep);
-            if (!container) container = helpers.findSpawn(creep);
-            if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(container);
-            }
+          var container = creep.findClosestContainerThatIsNotFull();
+          if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(container);
+          }
         }
 	}
 };
+
+
 
 module.exports = roleMiner;
