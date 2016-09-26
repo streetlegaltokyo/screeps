1979aa789421751b35bc9b331463e68dd9f8e80f
Street Legal <streetlegal@mydesignbuddy.com>
2016-09-26 09:30:44 -0500

    Round-robin repairing

--- role.repairer.js
+++ role.repairer.js
@@ -16,6 +16,7 @@ var roleRepairer = {
     run: function(creep) {
         if(creep.memory.repairing && creep.carry.energy == 0) {
             creep.memory.repairing = false;
+            creep.memory.structure = null;
         }
         if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
             creep.memory.repairing = true;
